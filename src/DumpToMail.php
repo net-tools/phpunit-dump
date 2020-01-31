8cb9cd5e3b3d0f63e0baa72ff20347b4cdd1be45
Pierre <dev@nettools.ovh>
2020-01-31 10:55:03 +0100

    no dependencies to net-tools/mailing|core anymore

--- src/DumpToMail.php
+++ src/DumpToMail.php
@@ -64,16 +64,40 @@ class DumpToMail extends DumpExtension
 						
 			// for each data dump
 			foreach ( $data as $k => $v )
-				$atts[] = array('file'=>$v, 'filename'=>$k, \Nettools\Core\Helpers\FileHelper::guessMimeType($k));
+				$atts[] = array('file'=>$v, 'filename'=>$k, 'application/octet-stream');
 
-			
-			// creating a Multipart mail with attachments
-			$mail = \Nettools\Mailing\Mailer::createText($this->_content);
-			$mail = \Nettools\Mailing\Mailer::addAttachments($mail, $atts, true, false);
 
+			$sep = "MailMultipart-mixed-" . sha1(uniqid());
+			$h = 	"From: " . $this->_from . 
+					"\r\n" .
+					"Content-Type: multipart/mixed;\r\n   boundary=\"$sep\"";           
 			
-			if ( $ret = \Nettools\Mailing\Mailer::getDefault()->sendmail($mail, $this->_from, $this->_recipient, 'PHPUnit data dump : ' . count($atts) . ' attachments') )
-				throw new \Exception($ret);
+			$m =	"--$sep" . 
+					"\r\n" .
+					"Content-Type: text/plain; charset=UTF-8\r\n" .
+						   "Content-Transfer-Encoding: quoted-printable" .
+					"\r\n" .
+					"\r\n" .
+					trim(str_replace("=0A", "\n", str_replace("=0D", "\r", imap_8bit($this->_content)))) .
+					"\r\n" . 
+					"\r\n";
+
+			foreach ( $atts as $att )
+			$m .="--$sep" .
+				 "\r\n" .
+				 "Content-Type: application/octet-stream;\r\n   name=\"" . $att['filename'] . "\"\r\n" .
+						 "Content-Transfer-Encoding: base64\r\n" .
+						 "Content-Disposition: attachment;\r\n   filename=\"" . $att['filename'] . "\"" .
+				 "\r\n" .
+				 "\r\n" .
+				 trim(chunk_split(base64_encode($att['file']))) .
+				 "\r\n" .
+				 "\r\n";
+
+			$m .= "--$sep--";
+
+			mail($this->_recipient, 'PHPUnit data dump : ' . count($atts) . ' attachments', $m, $h);
+			
 		}
 		catch (\Throwable $e)
 		{
